4f9f1182a10d40fc6ac38747df1725e24953f17f
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-10-23 13:56:09 -0400

    feat: :sparkles: config module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -39,22 +39,20 @@ export class AuthService {
     }
   }
   async signin(dto: AuthDto) {
-    try {
-      // find the user by email
-      const user = await this.prisma.user.findUnique({
-        where: {
-          email: dto.email,
-        },
-      });
-      // if user does not exist throw exception
-      if (!user) throw new ForbiddenException('Credentials incorrect');
-      // compare password
-      const pwMatches = await argon.verify(user.hash, dto.password);
-      // if password incorrect throw exception
-      if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
-      // send back the user
-      delete user.hash;
-      return user;
-    } catch (error) {}
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
