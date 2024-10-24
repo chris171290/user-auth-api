ca2cf655d47b1b6a2ec6310a525b3ef798fafd78
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-10-24 12:33:56 -0400

    feat: :sparkles: add user module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,6 +13,6 @@ export class PrismaService extends PrismaClient {
         },
       },
     });
-    console.log(config.get('DATABASE_URL'));
+    // console.log(config.get('DATABASE_URL'));
   }
 }
